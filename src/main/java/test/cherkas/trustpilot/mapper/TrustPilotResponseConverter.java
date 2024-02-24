3e0166727718a84604cc584348299e91c2078f3d
cherkas1dima <cherkas231095@gmail.com>
2024-02-24 19:57:03 +0300

    resolve conflict lombok/mapstruct

--- src/main/java/test/cherkas/trustpilot/mapper/TrustPilotResponseConverter.java
+++ src/main/java/test/cherkas/trustpilot/mapper/TrustPilotResponseConverter.java
@@ -2,7 +2,6 @@ package test.cherkas.trustpilot.mapper;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import test.cherkas.trustpilot.domain.TrustPilotResponse;
 import test.cherkas.trustpilot.domain.props.BusinessUnit;
